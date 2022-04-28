02b43a8460c8ba01a9ba91d2892924734b024b0d
JeongSooNa <skwjdtn3171@naver.com>
2022-04-28 16:30:19 +0900

    feat : stat

--- Project_statistics/JAVA/StaticsLogic.java
+++ Project_statistics/JAVA/StaticsLogic.java
@@ -1,72 +1,115 @@
 package statistics_project;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class StaticsLogic {
 	// 최솟값
-	public double min(double array[]) {
-		double min = array[0];
-		for(int i=0;i<array.length;i++) {
-			if(array[i]<min) min = array[i];
+	public float min(List<Float> array) {
+		float min = array.get(0);
+		for(int i=0;i<array.size();i++) {
+			if(array.get(i)<min) min = array.get(i);
 		}
 		return min;
 	}
 	// 최댓값
-	public double max(double array[]) {
-		double max = 0;
-		for(int i=0;i<array.length;i++) {
-			if(array[i]>max) max = array[i];
+	public float max(List<Float> array) {
+		float max = array.get(0);
+		for(int i=0;i<array.size();i++) {
+			if(array.get(i)>max) max = array.get(i);
 		}
 		return max;
 	}
 	// 합
-	public double sum(double array[]) {
-		double sum = 0;
-		for(double i:array) sum += i;
+	public float sum(List<Float> array) {
+		float sum = 0;
+		for(float i:array) sum += i;
 		return sum;
 	}
 	// 평균
-	public double avg(double array[]) {
-		double sum = 0;
-		for(double i:array) sum += i;
-		return sum/array.length;
+	public float avg(List<Float> array) {
+		float sum = 0;
+		for(float i:array) sum += i;
+		return sum/array.size();
 	}
 	// 중위수
-	public double cen(double array[]) {
-		Arrays.sort(array);
-		if(array.length%2 != 0) return array[array.length/2];
-		else return (array[array.length/2]+array[(array.length/2)-1])/2;
+	public float cen(List<Float> array) {
+		Collections.sort(array);
+		int len = array.size();
+		if(len%2==0) return (array.get(len/2)+array.get((len/2)-1))/2;
+		else return array.get(len/2);
 	}
 	// 표준편차
-	public double std(double array[]) {
-		double sum = 0;
-		for(double i:array) sum += i;
-		double avg = sum/array.length;
-		double stdSum = 0;
-		for(double i : array) {
+	public float std(List<Float> array) {
+		float sum = 0;
+		for(float i:array) sum += i;
+		float avg = sum/array.size();
+		float stdSum = 0;
+		for(float i : array) {
 			stdSum += (i-avg)*(i-avg);
 		}
-		return Math.sqrt(stdSum/(array.length-1));
+		return (float)(Math.sqrt(stdSum/(array.size()-1)));
 	}
 	// 분산
-	public double var(double array[]) {
-		double sum = 0;
-		for(double i:array) sum += i;
-		double avg = sum/array.length;
-		double stdSum = 0;
-		for(double i : array) {
+	public float var(List<Float> array) {
+		float sum = 0;
+		for(float i:array) sum += i;
+		float avg = sum/array.size();
+		float stdSum = 0;
+		for(float i : array) {
 			stdSum += (i-avg)*(i-avg);
 		}
-		return stdSum/(array.length-1);
+		return stdSum/(array.size()-1);
 	}
 	// 1사분위수
-	public double q1(double array[]) {
-		int len = array.length;
+	public float q1(List<Float> array) {
+		Collections.sort(array);
+		int len = array.size();
+		if(len%4 == 1) return array.get((len-1)/2/2);
+		if(len%4 == 2) return ((float)(array.get((len-2)/2/2)*0.25)+(float)(array.get(((len-2)/2/2)+1)*0.75))/2;
+		if(len%4 == 3) return (array.get((len-3)/2/2)+array.get(((len-3)/2/2)+1))/2;
+		if(len%4 == 0) return ((float)(array.get((len)/2/2)*0.75)+(float)(array.get(((len)/2/2)-1)*0.25))/2;
 		return 0;
 	}
 	// 3사분위수
-	public double q3(double array[]) {
-		
+	public float q3(List<Float> array) {
+		Collections.sort(array);
+		int len = array.size();
+		if(len%4 == 1) return array.get(3*(len/4));
+		if(len%4 == 2) return ((float)(array.get(3*(len/4))*0.75)+(float)(array.get((3*(len/4))+1)*0.25))/2;
+		if(len%4 == 3) return (array.get(((len/4)*3)+1)+array.get(((len/4)*3)+2))/2;
+		if(len%4 == 0) return ((float)(array.get(3*(len/4))*0.75)+(float)(array.get((3*(len/4))-1)*0.25))/2;
 		return 0;
 	}
+	// 사분위수 범위
+	public float iqr(List<Float> list) {
+		return q3(list)-q1(list);
+	}
+	// 이상치 추출
+	public List<Float> outlier(List<Float> list) {
+		List<Float> outlier = new ArrayList<Float>();
+		for(Float i : list) {
+			if(i > (q3(list)+(iqr(list)*3)) || i < (q1(list)-(iqr(list)*3))) {
+				outlier.add(i);
+			};
+		}
+		return outlier;
+	}
+	// 이상치를 제외한 List 추출
+	public List<Float> removeOutlier(List<Float> list){
+		List<Float> outlierIndex = new ArrayList<Float>();
+		for(int i=0;i<list.size();i++) {
+			if(i > (q3(list)+(iqr(list)*3)) || i < (q1(list)-(iqr(list)*3))) {
+				outlierIndex.add(i);
+			};
+		}
+		return null;
+	}
+	// List가 주어지고, 유의확률이 주어질 때 검정 결과(Boolean)
+	// List가 주어지고, 유의확률이 주어질 때 참이 될 수 있는 표본 수
+	// 표폰의 왜도, 첨도
+	// 정규성 검정
+	// List 두 개가 주어질 떄 상관계수, 상관분석
 }
